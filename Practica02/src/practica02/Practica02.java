11e2b191bab95c0311a602858ff53f6e736d698e
CESDE <CESDE@PC-26>
2018-10-10 08:31:15 -0500

    subiendo el proyecto inicial

--- Practica02/src/practica02/Practica02.java
+++ Practica02/src/practica02/Practica02.java
@@ -16,6 +16,10 @@ public class Practica02 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        // modificando el codigo fuente de mi aplicacion
+        
+        System.out.println("hola mundo");
     }
     
 }
